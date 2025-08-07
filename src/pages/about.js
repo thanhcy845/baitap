c948c075c85c10a9246eae0da393d0cdb900d303
thanhcy845 <thanhcy09@gmail.com>
2025-08-07 19:41:58 +0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,12 @@
-// src/pages/About.js
 import React from 'react';
 
 function About() {
-  return <h1>About Us Page</h1>;
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Về Chúng Tôi</h1>
+      <p>Đây là ứng dụng được tạo ra để minh họa React Router và quản lý state.</p>
+    </div>
+  );
 }
 
 export default About;
